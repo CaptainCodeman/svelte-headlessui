43979203b58b62a78e92a453266e66dee96621f1
Simon Green <simon@captaincodeman.com>
2023-06-25 09:36:56 -0600

    handle clickOutside after drag
    track initial pointer target, only close if it was an outside click
    remove unused `when`
    closes #59

--- packages/lib/src/lib/internal/on-click-outside.ts
+++ packages/lib/src/lib/internal/on-click-outside.ts
@@ -1,21 +1,33 @@
 import type { Behavior } from "./behavior"
 import { listener } from "./events"
 
-export function onClickOutside(fn: (event: Event) => void, when?: () => boolean): Behavior {
+export function onClickOutside(fn: (event: Event) => void): Behavior {
   return node => {
+    let initial: Node | null = null
+
     function handler(event: Event) {
       if ((event as PointerEvent).pointerType === '') return // ignore space as click
-      if (!when || when()) {
-        if (!node.contains(event.target as Node)) {
-          if (node.clientWidth) {
-            event.preventDefault()
-            event.stopPropagation()
-            fn(event)
-          }
+      if (initial && !node.contains(initial)) {
+        if (node.clientWidth) {
+          event.preventDefault()
+          event.stopPropagation()
+          fn(event)
         }
+      }
+      initial = null
+    }
+
+    function setInitial(event: PointerEvent) {
+      if (event.isPrimary) {
+        initial = event.target as Node
       }
     }
 
-    return listener(document.documentElement, 'click', handler, true)
+    const listeners = [
+      listener(document.documentElement, 'pointerdown', setInitial, true),
+      listener(document.documentElement, 'click', handler, true),
+    ]
+
+    return () => listeners.forEach(listener => listener())
   }
 }
