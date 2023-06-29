ce9bcf6a19a268307c2190cc25c76883b73e16b6
nbe <aleisa.naser@gmail.com>
2023-06-29 11:20:04 +0300

    chore: remove vitest config

--- packages/lib/vite.config.js
+++ packages/lib/vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vitest/config'
-import { sveltekit } from '@sveltejs/kit/vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		setupFiles: ['src/tests/setup'],
-		coverage: {
-			exclude: ['src/tests/**/*'],
-		},
-	},
-})
+});
