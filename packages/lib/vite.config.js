7b94b5ab5c33628ef27cbe29941465ccef13f51e
Simon Green <simon@captaincodeman.com>
2023-08-11 10:49:46 -0600

    remove test dependencies

--- packages/lib/vite.config.js
+++ packages/lib/vite.config.js
@@ -3,13 +3,4 @@ import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		include: ['src/**/*.{test,spec}.{js,ts}'],
-		setupFiles: ['src/tests/setup'],
-		coverage: {
-			exclude: ['src/tests/**/*'],
-		},
-	},
 })
