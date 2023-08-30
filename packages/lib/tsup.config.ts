c0ed7c6fa6ebda01453d275c0bc29a5515b52f51
Simon Green <simon@captaincodeman.com>
2023-08-30 13:53:35 -0600

    shave some bytes

--- packages/lib/tsup.config.ts
+++ packages/lib/tsup.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/lib/index.ts'],
   format: ['esm'],
-  external: ['svelte/store'],
+  external: [
+    'svelte',
+    'svelte/store',
+  ],
   splitting: false,
   sourcemap: true,
   minify: true,
