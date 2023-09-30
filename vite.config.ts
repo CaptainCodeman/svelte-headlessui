c7356e97458a66ee1095d92f7eff0bcb0eaec230
Simon Green <simon@captaincodeman.com>
2023-09-30 13:34:28 -0600

    update svelte-doc-kit

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,5 @@ import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: [
-			'svelte-doc-kit',
-			'unified',
-			'remark-parse',
-			'remark-gfm',
-			'remark-frontmatter',
-			'remark-directive',
-			'remark-rehype',
-			'remark-gemoji',
-			'remark-github',
-			'rehype-highlight',
-			'rehype-stringify',
-			'rehype-preset-minify',
-			'rehype-raw',
-			'vfile-matter',
-			'unist-util-visit',
-			'hastscript',
-			'lowlight',
-		]
-	}
+	plugins: [sveltekit()]
 })
