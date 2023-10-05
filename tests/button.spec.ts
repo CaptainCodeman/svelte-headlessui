aecc2253ba7f01f1dc568ef96c0b5bd24453be87
Simon Green <simon@captaincodeman.com>
2023-10-05 16:05:22 -0600

    playwright tests

---
+++ tests/button.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('toggle button', async ({ page }) => {
+  await page.goto('/example/button');
+
+  const button = page.getByRole('button');
+
+  expect(button).toHaveAttribute('aria-label', 'Control Music')
+  expect(button).toHaveAttribute('aria-pressed', 'false')
+  expect(button).toHaveText('Play')
+  await expect(page).toHaveScreenshot('unpressed.png');
+
+  await button.click()
+
+  expect(button).toHaveText('Pause')
+  expect(button).toHaveAttribute('aria-pressed', 'true')
+  await expect(page).toHaveScreenshot('pressed.png');
+});
